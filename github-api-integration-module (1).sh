19f7d4e29804eb910ea7e5fd4c4f565f616ed76a
sairahul026 <142714610+sairahul026@users.noreply.github.com>
2023-09-19 12:26:46 -0700

    Update github-api-integration-module (1).sh

--- github-api-integration-module (1).sh
+++ github-api-integration-module (1).sh
@@ -1,47 +1,30 @@
 #!/bin/bash
 ################################
-# Author: Abhishek
-# Version: v1
-#
-#
-#
-# This script will help users to communicate and retrieve information from GitHub
-# Usage:
-#   Please provide your github token and rest api to the script as input
-#
+below if statement is used to check a condition that user should give atleast two parameters
+i.e github token and rest api url $0 indicates the present script
 ################################
 
 if [ ${#@} -lt 2 ]; then
-    echo "usage: $0 [your github token] [REST expression]"
+    echo "usage: $0 [enter your github token] [provide REST api expression]"
     exit 1;
 fi
 
+#there vairables takes the user input values as parameters
 GITHUB_TOKEN=$1
 GITHUB_API_REST=$2
 
+#thebekow expression is used to get information from github in json format
 GITHUB_API_HEADER_ACCEPT="Accept: application/vnd.github.v3+json"
 
 temp=`basename $0`
+#below commands creates a temporary file to store the output details of this script
 TMPFILE=`mktemp /tmp/${temp}.XXXXXX` || exit 1
 
-
+#defining a function for api call
 function rest_call {
     curl -s $1 -H "${GITHUB_API_HEADER_ACCEPT}" -H "Authorization: token $GITHUB_TOKEN" >> $TMPFILE
 }
 
-# single page result-s (no pagination), have no Link: section, the grep result is empty
-last_page=`curl -s -I "https://api.github.com${GITHUB_API_REST}" -H "${GITHUB_API_HEADER_ACCEPT}" -H "Authorization: token $GITHUB_TOKEN" | grep '^Link:' | sed -e 's/^Link:.*page=//g' -e 's/>.*$//g'`
-
-# does this result use pagination?
-if [ -z "$last_page" ]; then
-    # no - this result has only one page
-    rest_call "https://api.github.com${GITHUB_API_REST}"
-else
-
-    # yes - this result is on multiple pages
-    for p in `seq 1 $last_page`; do
-        rest_call "https://api.github.com${GITHUB_API_REST}?page=$p"
-    done
-fi
+printing the output in tempfile using cat command
 
 cat $TMPFILE
